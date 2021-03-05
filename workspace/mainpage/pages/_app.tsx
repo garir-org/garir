d1835050ae18a43c822ac8f9a198abbbf842b569
Chris <christianto.g.14@gmail.com>
2021-03-05 20:38:04 +0700

    install twin + emotion, prepare for Nextjs integration

---
+++ workspace/mainpage/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyles } from 'twin.macro'
+
+function MyApp({ Component, pageProps }) {
+  return <div>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </div>
+}
+
+export default MyApp
